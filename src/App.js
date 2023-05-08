8fccc65163f508424eca5a9c13fa04dc62e968c9
shravya-gif <95952294+shravya-gif@users.noreply.github.com>
2023-05-08 18:40:28 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+//import {ArrowRight} from 'react-bootstrap-icons';
+import Sidebar from './Components/Sidebar'
+import Home from './Components/Home'
+import { useState } from 'react'
+import 'bootstrap-icons/font/bootstrap-icons.json'
+import 'react-bootstrap-icons'
+//  import { MDBCol, MDBIcon } from "mdbreact";
+// import { i } from 'react-bootstrap-icons'
+// import 'react-bootstrap-icons/dist/bootstrap-icons'
+// import * as i from 'react-bootstrap-icons'
+
+
 
 function App() {
+  const [toggle, setToggle] = useState(true)
+  const Toggle = () =>{
+    setToggle(!toggle)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid bg-secondary min-vh-100'>
+      <i/>
+      {/* <ArrowRight/> */}
+      <div className='row'>
+        {toggle && <div className='col-4 col-md-2 bg-white vh-100 position-fixed'>
+      <Sidebar/>
+      </div>}
+      {toggle && <div className='col-4 col-md-2'></div>}
+      <div className='col'>
+        <Home Toggle={Toggle}/>
+        </div>
+      </div>
     </div>
-  );
+    
+  )
+  
+
+
+// const SearchPage = () => {
+//   return (
+//     <MDBCol md="6">
+//       <form className="form-inline mt-4 mb-4">
+//         <MDBIcon icon="search" />
+//         <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" />
+//       </form>
+//     </MDBCol>
+//   );
+// }
+
+
 }
 
-export default App;
+ export default App
+ 
+  
+ 
